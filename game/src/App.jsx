4e6bc9c866e039d1c6e3e3df09a1314dfd445d3b
Ugisele <Umutonigisele12@gmail.com>
2024-07-11 21:35:57 +0200

    New application with react

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,50 +1,44 @@
-import Home from './components/home';
-import { Outlet, Route, BrowserRouter, Routes } from 'react-router-dom';
-import Navig from './components/Navig';
-import Footer from './components/Footer';
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
 
+function App() {
+  // const [count, setCount] = useState(0)
 
-
-
-
-const Layout = () => {
   return (
     <>
-      <Navig />
-        <Outlet />
-      <Footer />
+      <div>
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+        <h2>Programming languages</h2>
+
+        <ul>
+          <li>Java</li>
+          <li>Python</li>
+          <li>PHP</li>
+          <li>Javascript</li>
+          
+        </ul>
+      </div>
+      {/* <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
     </>
   )
 }
-function App() {
-  return (
-
-  <>
-  <BrowserRouter>
-    <Routes>
-
-      <Route path='/' element={<Layout/>}>
-
-        <Route path='home' element={<Home/>} />
-        <Route path='about' element={<About/>} />
-        <Route path='contact' element={<Contact />} /> 
-
-      </Route>
-    </Routes>
-  </BrowserRouter>
-  </>
-  )
-
-
-}
 
 export default App
-// import React from 'react'
-
-// function App() {
-//   return (
-//     <div style={{flex:1, backgroundColor:'red'}}>App</div>
-//   )
-// }
-
-// export default App
