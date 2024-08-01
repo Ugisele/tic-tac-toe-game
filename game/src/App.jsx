cf0f0a3605fc54a671e4d70201ffdc868a00c34e
Ugisele <Umutonigisele12@gmail.com>
2024-08-01 12:27:04 +0200

    Add new realese page

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Trailer from "./component/card";
 import Home from "./home";
+import Navigation from "./Navigation";
 
 function App() {
   return (
     <>
+    <Navigation/>
      <Home/>
      <Trailer/>
     </>
--- game/src/component/card.jsx
+++ game/src/component/card.jsx
@@ -1,11 +1,14 @@
 // import React from "react";
 // import bg1 from '.bg1.png';
+import "../App";
+import bg1 from '../assets/bg1.png';
+
 
 const Trailer = () => {
     return(
-        <div className='flex-col flex p-6 bg-black h-[auto]'>
+        <div className='flex-col flex p-6 bg-red-400'>
         <div className=" ">
-          <image src= "../../public/bg1.png" alt="movies" className="w-44 h-44"/>
+          <img src= {bg1} alt="movies" style={{ height:'110px'}} className="w-32"/>
           </div>
           <div className="flex-col">
           <h2 className="text-2xl text-white">The flash </h2>
