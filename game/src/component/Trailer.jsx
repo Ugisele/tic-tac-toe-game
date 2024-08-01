0db5f4098812da6b60c5e8998d7308fec1296b87
Ugisele <Umutonigisele12@gmail.com>
2024-08-01 15:11:34 +0200

    Add trending page

--- game/src/component/Trailer.jsx
+++ game/src/component/Trailer.jsx
@@ -1,24 +1,20 @@
 // import React from "react";
-// import bg1 from '.bg1.png';
 import "../App";
-import bg1 from '../assets/bg1.png';
+// import bg1 from '../assets/bg1.png';
 
 
-const Trailer = ({img,title,description,rate}) => {
+const Trailer = ({img,title,description,date}) => {
     return(
-        <div>
-             <div className='flex-row flex p-6 bg-red-400 gap-2'>
+             <div className='flex gap-2 '>
                 <div className=" ">
-                <img src= {bg1} alt="movies" className="w-12 h-16 rounded-md"/>
+                <img src= {img} alt="" className="w-16 h-24 rounded-md"/>
                 </div>
-                <div className="flex-col justify-center pt-1">
-                <h2 className="text-sm text-white">The flash </h2>
-                <p className="text-white text-xs">Series </p>
-                <p className="text-white text-xs">rate</p>
+                <div className="flex-col justify-center pt-3">
+                <h2 className="text-md text-white">{title} </h2>
+                <p className="text-white text-xs">{description} </p>
+                <p className="text-white text-xs">{date}</p>
                 </div>
             </div>
-        </div>
-        
     )
 }
 export default Trailer;
---
+++ game/src/component/Trending.jsx
@@ -0,0 +1,10 @@
+const Trending = ()=>{
+    return(
+        <div>
+            <h1 className="text-white text-2xl">Trending</h1>
+          
+
+        </div>
+    )
+}
+export default Trending;
